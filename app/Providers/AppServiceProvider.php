0f72dc51d61295d5589e2f3b4771443d5bbbcbfd
Restu Reese <restureese@gmail.com>
2023-12-13 14:38:45 +0700

    fix: https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
 
         if (env('APP_ENV') === 'production') {
             \URL::forceRootUrl(config('app.url'));
-            // \URL::forceScheme('https');
+            \URL::forceScheme('https');
         }
     }
 }
